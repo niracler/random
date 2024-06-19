dacab6d47357c1bb382f4ec7e96dac4eaea1ed37
niracler <i@niracler.com>
2024-06-19 17:37:51 +0800

    chore: import trello

--- random_dare.py
+++ random_dare.py
@@ -1,1 +1,14 @@
-print("Hello world!")
+
+import os
+import trello
+
+trello_client = trello.TrelloClient(
+    api_key=os.getenv('TRELLO_API_KEY')
+    api_secret=os.getenv('TRELLO_API_SECRET')
+    token=os.getenv('TRELLO_TOKEN')
+)
+
+all_boards = trello_client.list_boards()
+last_board = all_boards[0]
+print(last_board.name)
+print(all_boards[0].list_lists()[3].id)
